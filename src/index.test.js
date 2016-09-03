77eff561cf6852448892ff0eaddd2e66a6df8d2c
Vargentum <vargentum@gmail.com>
2016-09-03 12:04:06 +0300

    add real tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,32 @@
 var mocha = require('mocha')
   , expect = require('chai').expect
+  , starWarsNames = require('./index.js')
 
+describe('starwars-names', function() {
+  var allNames = starWarsNames.all
 
-describe('Setup', function() {
-  var actual = true
-  var expected = true
-
-  it('should work', function() {
-    expect(expected).to.equal(actual)
+  describe('all', function () {
+    function checkItemsForStringType (ary) {
+      return ary.every(function(item) {return typeof item === 'string'})
+    }
+  
+    it('should provide an array of items', function() {
+      expect(allNames).to.be.an('array')
+    })
+    it('should contain only string items', function() {
+      expect(allNames).to.satisfy(checkItemsForStringType)
+    })
+    it('should always contains "Luke Skywalker"', function() {
+      expect(allNames).to.include("Luke Skywalker")
+    })
   })
+
+  describe('random', function() {
+    var actual = starWarsNames.random()
+  
+    it('should return a random item from allNames', function() {
+      expect(allNames).to.include(actual)
+    })
+  })
+  
 })
